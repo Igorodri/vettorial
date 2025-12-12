4e83d8c8b52378012df8bc1c565f7c3e7d25874c
Igormascarenhas08 <igor.mascarenha@fbest.org.br>
2025-12-12 11:39:49 -0300

    raw - Alteração no design da seção de serviços

--- src/app/sections/servicos-section/servicos-section.ts
+++ src/app/sections/servicos-section/servicos-section.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
 
 @Component({
   selector: 'vettorial-servicos',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './servicos-section.html',
   styleUrl: './servicos-section.css',
 })
 export class ServicosSection {
-
+  faCheck = faCheck
 }
